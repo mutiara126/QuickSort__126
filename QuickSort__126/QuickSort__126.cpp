2094dcc054b9edb13908a829368e4d5dc9075dd2
mutiara126 <salsamutiara1812@gmail.com>
2023-03-30 20:14:28 +0800

    membuat prosedur swap

--- QuickSort__126/QuickSort__126.cpp
+++ QuickSort__126/QuickSort__126.cpp
@@ -4,7 +4,9 @@ using namespace std;
 
 int arr[20];            //Array of integers to hold values
 int cmp_count = 0;      //Number of comparison
+int mov_count = 0;      //Number of data movements
 
+	
 
 
 
