5cf94c57c786e74c0ef7e92556c02c51b6d26473
mutiara126 <salsamutiara1812@gmail.com>
2023-03-30 20:20:44 +0800

    membuat prosedur input

--- QuickSort__126/QuickSort__126.cpp
+++ QuickSort__126/QuickSort__126.cpp
@@ -5,6 +5,30 @@ using namespace std;
 int arr[20];            //Array of integers to hold values
 int cmp_count = 0;      //Number of comparison
 int mov_count = 0;      //Number of data movements
+int n;
+
+void input() {
+	while (true)
+	{
+		cout << "Masukkan panjang element array";
+			cin >> n;
+
+			if (n <= 20)
+				break;
+			else
+				cout << "\nMaksimum panjang array adalah 20" << endl;
+	}
+
+	cout << "\n-------------------" << endl;
+	cout << "\nEnter Array Element" << endl;
+	cout << "\n-------------------" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">"
+			cin >> arr[i]; 
+	} 
+
 
 	
 
